b88752716c4cc31ebcd1744041733d205f2c3d76
souvikchat4u1981 <souvik.chatterjee1981@gmail.com>
2024-02-09 12:07:46 +0530

    Updates

---
+++ appConfig.js
@@ -0,0 +1,6 @@
+var Configs = {
+  baseaddress: "http://localhost:8080/libraryAPI/api/",
+  graphqlAddress: "http://localhost:8080/libraryAPI/graphql",
+  baseName: "/",
+  applicationName: "Souvik's Library",
+};
